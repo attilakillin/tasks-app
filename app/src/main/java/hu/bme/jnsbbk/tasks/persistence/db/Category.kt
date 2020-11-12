4841a6a581818fd5c321133a3b4ad896200da7ad
Attila Killin <43002846+attilakillin@users.noreply.github.com>
2020-11-12 20:06:47 +0100

    Default null values for ids

--- app/src/main/java/hu/bme/jnsbbk/tasks/persistence/db/Category.kt
+++ app/src/main/java/hu/bme/jnsbbk/tasks/persistence/db/Category.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "categories")
 data class Category(
-    @PrimaryKey(autoGenerate = true) var cat_id: Long?,
+    @PrimaryKey(autoGenerate = true) var cat_id: Long? = null,
     @ColumnInfo(name = "name") var name: String,
     @ColumnInfo(name = "color_light") var color_light: String,
     @ColumnInfo(name = "color_dark") var color_dark: String
